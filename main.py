cef2350b5380fba4b1088defb7d92b180915dba5
Vik <vikoliveira.dev@gmail.com>
2025-10-27 09:43:07 -0300

    Criação da tabela no Html e estilização no Css

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+#pip install fastapi uvicorn jinja2
+#jinja2 = auxilia a fazer a conexão entre python e html
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
-app = FastAPI("API de Alunos")
+app = FastAPI(title="API de alunos")
 
-templates = Jinja2Templates(directory="tamplates")
+#Configurar o diretório dos nossos templates jinja2
+templates = Jinja2Templates(directory="templates")
 
-#puxar arquivos css
-app.mount("/static", StaticFiles(directory="static"), name="static")
+#Pasta static para servir os arquivos (CSS, Imagens ou JS)
+app.mount("/static", StaticFiles(directory="static"), name= "static")
+
 
 alunos = [
-    {"nome": "Felipe", "nota": 8.5},
-    {"nome": "Laura", "nota": 5.0}
-    ]
+    {"nome":"Felipe","nota": 8.5},
+    { "nome": "Laura","nota": 9.0},
+    {"nome":"Yasmin","nota":8.0}
+]
 
+
+#Rota inicial
 @app.get("/", response_class=HTMLResponse)
-def exibir_aluno(request: Request):
+def exibir_alunos(request: Request):
     return templates.TemplateResponse(
-        "alunos.html", {"request": Request, "lista_alunos": alunos}
+        "Alunos.html", {"request":request, "lista_alunos": alunos}
     )
